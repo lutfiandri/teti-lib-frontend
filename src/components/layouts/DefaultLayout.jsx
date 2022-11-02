87d6361da9d571f962e33002dfdfa5d4a599f866
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-11-02 07:59:39 +0700

    feat(setup): add react helmet to add html head

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -1,8 +1,13 @@
 import { Navbar } from "@/components/templates/Navbar";
+import { Helmet } from "react-helmet";
 
 export function DefaultLayout({ children, title = "TETI Library" }) {
   return (
     <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
       <Navbar></Navbar>
       <main>{children}</main>
     </>
