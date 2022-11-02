42ff70e65ebd90abb5daf5ed979582ff9487488c
BetNares <btari.f.r@mail.ugm.ac.id>
2022-11-02 23:24:39 +0700

    feature: nambahin box-box buku

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,174 @@
 import { DefaultLayout } from "@/components/layouts/DefaultLayout";
+import {
+    Box, Container, SimpleGrid, Grid, GridItem, Image, Text,
+    useDisclosure,
+    useConst,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from "@chakra-ui/react";
 
 export function Home() {
-  return <DefaultLayout>Home Page</DefaultLayout>;
+    //const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        < DefaultLayout >
+
+            <Box bg='orange.100' w='100%' p={5}>
+
+                <SimpleGrid minChildWidth='236px' spacing={5}>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                    <Box borderRadius='10' bg='white' width='236px' height='391px' >
+                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                            <image></image>
+                        </Box>
+                        <Box bg='white' w='190px' h='40px' m={6}>
+                            <Text>
+                                BOOK TITLE
+                            </Text>
+                            <Text fontSize='12'>
+                                AUTHOR
+                            </Text>
+                        </Box>
+                    </Box>
+
+                   
+                </SimpleGrid>
+
+            </Box>
+        </DefaultLayout >
+        
+    )
+    
 }
+
