87be342bb4e90f2588a70e5768d5752af25172c2
junianN <rizky.j.n@mail.ugm.ac.id>
2022-11-11 12:38:29 +0700

    feat: useFetch and books map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,89 +23,97 @@ import {
 
 import React from "react";
 
+import useFetch from "@/useFetch";
 
 export function Home() {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
     const toast = useToast()
 
-    return (
-        < DefaultLayout >
-
-            <Box bg='orange.100' w='100%' p={5}>
-
-                <SimpleGrid minChildWidth='236px' spacing={5}>
-
-                    <Box onClick={onOpen} cursor='pointer' borderRadius='10' bg='white' width='236px' height='391px' >
-                        <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
-                            <image></image>
-                        </Box>
-                        <Box bg='white' w='190px' h='40px' m={6}>
-                            <Text>
+    const { error, isLoading, data: books} = useFetch('http://localhost:5000/books'); 
+    if(books) {
+        return (
+            < DefaultLayout >
+    
+                <Box bg='orange.100' w='100%' p={5}>
+    
+                    <SimpleGrid minChildWidth='236px' spacing={5}>
+    
+                        
+                        {books.data.books.map((book) => (
+                        <Box onClick={onOpen} cursor='pointer' borderRadius='10' bg='white' width='236px' height='391px' key={book._id}>
+                            <Box borderRadius='10' bg='lightgray' w='190px' h='249px' m={6}>
+                                <image></image>
+                            </Box>
+                            <Box bg='white' w='190px' h='40px' m={6}>
+                                <Text>
+                                    { book.title }
+                                </Text>
+                                <Text fontSize='12'>
+                                    { book.author}
+                                </Text>
+                            </Box>
+                        </Box>    
+                    ))}
+    
+                    </SimpleGrid>
+    
+                </Box>
+    
+                <Modal isOpen={isOpen} onClose={onClose} isCentered scrollBehavior='inside'>
+                    <ModalOverlay></ModalOverlay>
+                    <ModalContent>
+                        <ModalHeader> Book Details </ModalHeader>
+                        <ModalCloseButton></ModalCloseButton>
+                        <ModalBody>
+                            <Box borderRadius='10' bg='lightgray' h='473px' m={5 } >
+                                <image></image>
+                            </Box>
+                            <Text fontSize='24' marginBottom={2}>
                                 BOOK TITLE BOOK TITLE BOOK TITLE
                             </Text>
-                            <Text fontSize='12'>
-                                AUTHOR AUTHOR AUTHOR
+                            <Text marginBottom={2} fontSize='16'>
+                                by AUTHOR AUTHOR AUTHOR
                             </Text>
-                        </Box>
-                    </Box>
-
-                </SimpleGrid>
-
-            </Box>
-
-            <Modal isOpen={isOpen} onClose={onClose} isCentered scrollBehavior='inside'>
-                <ModalOverlay></ModalOverlay>
-                <ModalContent>
-                    <ModalHeader> Book Details </ModalHeader>
-                    <ModalCloseButton></ModalCloseButton>
-                    <ModalBody>
-                        <Box borderRadius='10' bg='lightgray' h='473px' m={5 } >
-                            <image></image>
-                        </Box>
-                        <Text fontSize='24' marginBottom={2}>
-                            BOOK TITLE BOOK TITLE BOOK TITLE
-                        </Text>
-                        <Text marginBottom={2} fontSize='16'>
-                            by AUTHOR AUTHOR AUTHOR
-                        </Text>
-
-                        <Text fontSize='16'>
-                            BOOK SYNOPSIS Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris suscipit diam in leo congue congue. Aliquam hendrerit eget purus
-                            a consequat. Fusce elit lectus, ornare vitae diam quis, cursus porta eros.
-                            Suspendisse est leo, rhoncus vel euismod nec, vulputate sed odio. Vestibulum
-                            varius purus erat, ac commodo leo hendrerit quis. Curabitur nibh dolor, euismod
-                            id nunc vitae, ornare mattis massa. Nulla facilisi.
-                        </Text>
+    
+                            <Text fontSize='16'>
+                                BOOK SYNOPSIS Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Mauris suscipit diam in leo congue congue. Aliquam hendrerit eget purus
+                                a consequat. Fusce elit lectus, ornare vitae diam quis, cursus porta eros.
+                                Suspendisse est leo, rhoncus vel euismod nec, vulputate sed odio. Vestibulum
+                                varius purus erat, ac commodo leo hendrerit quis. Curabitur nibh dolor, euismod
+                                id nunc vitae, ornare mattis massa. Nulla facilisi.
+                            </Text>
+                
+                        </ModalBody>
+    
+                        <ModalFooter>
+                            <Button colorScheme='teal' mr={3}
+                                onClick={() =>
+                                    toast({
+                                        title: 'Borrowed',
+                                        description: "Book saved into My library.",
+                                        status: 'success',
+                                        duration: 9000,
+                                        isClosable: true,
+                                    })
+                                }
+                            >
+                                Borrow
+                            </Button>
+                            <Button isDisabled colorScheme='teal'>
+                                Return
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+    
+                </Modal>
+    
+            </DefaultLayout >
             
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='teal' mr={3}
-                            onClick={() =>
-                                toast({
-                                    title: 'Borrowed',
-                                    description: "Book saved into My library.",
-                                    status: 'success',
-                                    duration: 9000,
-                                    isClosable: true,
-                                })
-                            }
-                        >
-                            Borrow
-                        </Button>
-                        <Button isDisabled colorScheme='teal'>
-                            Return
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-
-            </Modal>
-
-        </DefaultLayout >
-        
-    )
+        )
+    }
+    
 
     
 }
